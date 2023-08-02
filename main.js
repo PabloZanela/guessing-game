a66e9fbf000b357d7b93de99368922590c5de180
“Pablo <pablozp01@hotmail.com>
2023-08-02 19:26:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//Variáveis
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+//Eventos
+  btnTry.addEventListener('click', handleTryClick)
+  btnReset.addEventListener('click', handResetClick)
+  document.addEventListener('keydown', handKeydown)
+
+//funções
+function handleTryClick(event) {
+  event.preventDefault()
+
+  const inputNumber = document.querySelector('#inputNumber')
+
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector(".screen2 h2").innerText = `acertou em ${xAttempts} tentativas`
+  }
+
+  inputNumber.value = ""
+  xAttempts++
+
+}
+function handResetClick(){
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function handKeydown(){
+    if(e.key == 'Enter' && screen1.classList.contains('hide'))
+    handleResetClick()
+}
+
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+
+
